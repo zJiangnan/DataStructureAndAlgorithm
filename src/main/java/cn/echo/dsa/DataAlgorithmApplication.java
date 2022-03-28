b6369827eb73199b25e8c1f833818f82384efe87
Unicorn <13669036625@163.com>
2022-03-28 16:57:07 +0800

    feat(项目构建): 张江楠
    构建项目

---
+++ src/main/java/cn/echo/dsa/DataAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package cn.echo.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAlgorithmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/echo/dsa/DataAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.echo.dsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
